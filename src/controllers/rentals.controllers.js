f6db5e4320483122f829e69f183f096b1a36feaa
Alice <aliceramosfer@gmail.com>
2025-09-22 16:18:57 -0300

    arrumado a importacao da funcao formatDate do arquivo rental.controllers.js

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,5 +1,10 @@
 import { rentalsServices } from "../services/rentals.servicies.js";
+<<<<<<< HEAD
 import { formatDate } from "../utils/fomateDate.js";
+=======
+import { formatDate } from "../utils/formatDate.js";
+
+>>>>>>> 2a6b02f (arrumado a importacao da funcao formatDate do arquivo rental.controllers.js)
 
 
 export async function getRentals(req,res) {
@@ -8,11 +13,19 @@ export async function getRentals(req,res) {
         ...rental,
         rentDate: formatDate(rental.rentDate),
         returnDate: rental.returnDate ? formatDate(rental.returnDate) : null,
+<<<<<<< HEAD
         customer: {        // sobrescreve ou cria o campo customer
             id: rental.customerId,
             name: rental.customer
         },
         game: {            // sobrescreve ou cria o campo game
+=======
+        customer: {        
+            id: rental.customerId,
+            name: rental.customer
+        },
+        game: {            
+>>>>>>> 2a6b02f (arrumado a importacao da funcao formatDate do arquivo rental.controllers.js)
             id: rental.gameId,
             name: rental.game
         }
--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,9 +1,17 @@
 export function formatDate(date) {
+<<<<<<< HEAD
   if (!date) return null; // caso seja null no banco
   const d = new Date(date);
 
   const ano = d.getFullYear();
   const mes = String(d.getMonth() + 1).padStart(2, "0"); // meses começam do 0
+=======
+  if (!date) return null;
+  const d = new Date(date);
+
+  const ano = d.getFullYear();
+  const mes = String(d.getMonth() + 1).padStart(2, "0"); 
+>>>>>>> 2a6b02f (arrumado a importacao da funcao formatDate do arquivo rental.controllers.js)
   const dia = String(d.getDate()).padStart(2, "0");
 
   return `${ano}-${mes}-${dia}`;
